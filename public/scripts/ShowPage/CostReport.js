8f784d93a46b1dc95468b87851a27e3f576a635f
Torin O'Regan-Latarius <torin.olat@macusers-MacBook-Air.local>
2021-09-29 22:32:47 -0700

    allow any access origin 11

--- public/scripts/ShowPage/CostReport.js
+++ public/scripts/ShowPage/CostReport.js
@@ -241,9 +241,9 @@ saveData=(reload=false, estimateVersion=_show.costReport.estimateVersion) => {
             }
         })
     })
-        .then(response => { return response.json(); })
+        .then(response => { console.log(response); return response.json(); })
         .then(responseData => {
-            console.log(responseData)
+
             if (reload) { location.reload() }
             else {
                 // Update saveStatus
