566e940db2d1aab6d26c17b6ebf3c6cd7eb54d60
Torin O'Regan-Latarius <torin.olat@macusers-MacBook-Air.local>
2021-09-29 20:20:51 -0700

    allow any access origin 7

--- utils/customMiddleware.js
+++ utils/customMiddleware.js
@@ -26,5 +26,6 @@ module.exports.isShowOwner=async (req, res, next) => {
 module.exports.allowAnyAccessOrigin=(req, res, next) => {
     res.set('Access-Control-Allow-Origin', '*')
     res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    res.set('type', 'basic')
     next()
 }
