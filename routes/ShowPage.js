e0949c6747c9bb1a42af790505df5ed29e9a6643
Torin O'Regan-Latarius <torin.olat@macusers-MacBook-Air.local>
2021-10-02 13:33:15 -0700

    ts gen as worker

--- routes/ShowPage.js
+++ routes/ShowPage.js
@@ -126,16 +126,13 @@ router.put('/', isLoggedIn, upload.single('file'), tryCatch(async (req, res, nex
     // Else queue it for a worker to process (production)
     else {
         const tsGenQueue=new Queue('tsGenQueue', process.env.REDIS_URL)
-        filepath=`${req.file.path}.xlsx`
-        console.log(`\n\n\n${filepath}\n\n\n`)
-        tsGenQueue.empty().then(async function () {
-            const job=await tsGenQueue.add({
-                show,
-                valueMap: cellValueMap,
-                filepath,
-                week,
-                filename: req.file.filename
-            })
+        filepath=`/${req.file.path}.xlsx`
+        const job=await tsGenQueue.add({
+            show,
+            valueMap: cellValueMap,
+            filepath,
+            week,
+            filename: req.file.filename
         })
     }
 
