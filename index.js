fa1f94ec5ac6482c6158ac5cc19c23fc27c7ac55
Torin O'Regan-Latarius <torin.olat@macusers-MacBook-Air.local>
2021-10-08 00:36:51 -0700

    fixing timesheet generation

--- index.js
+++ index.js
@@ -131,9 +131,8 @@ if (process.env.NODE_ENV=='production') {
     console.log(`\n\n Creating Completion Queue Listener\n\n`)
     const tsGenQueue=new Queue('tsGenQueue', process.env.REDIS_URL)
     tsGenQueue.on('global:completed', (job, result) => {
-        console.log(`\n\n\n Job ${job.id} Complete!\n\n\n`)
-        console.log(result)
-        global.generatedTimesheets.push(job.data.filename)
+        console.log(`\n\n\n Job ${job} Complete!\n\n\n`)
+        global.generatedTimesheets.push(result)
     })
 }
 
