c5b3819ec7555a970b32a903a0395d1bc472b2eb
Torin O'Regan-Latarius <torin.olat@macusers-MacBook-Air.local>
2021-12-17 00:02:22 -0800

    testing on clapper.ca

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const secret=process.env.SECRET
 const sessionConfig={
     store: MongoStore.create({ mongoUrl: dbUrl, touchAfter: 3600*24, secret: secret }),
     name: 'filmApp_session',
-    httpOnly: true,
+    //httpOnly: true,
     secure: true,
     secret: secret,
     resave: false,
